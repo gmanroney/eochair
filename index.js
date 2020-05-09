dbc9b61bdb4f15d56ee3b68b2b0c8704a9f8dcd5
Gerard Moroney <gerard.x.moroney@gmail.com>
2020-05-09 12:47:59 +0100

    second commit

--- index.js
+++ index.js
@@ -3,4 +3,8 @@ const app = express();
 app.get('/', (req,res) => {
 	res.send({ hi: 'theres' });
 });
-app.listen(5000);
+//const PORT = process.env.PORT || 5000
+//app.listen(PORT);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
